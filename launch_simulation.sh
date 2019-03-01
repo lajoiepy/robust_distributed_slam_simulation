fc924d0c04aa997cb3a8e67eda03218a4c1eca78
Pierre-Yves Lajoie <lajoie.py@gmail.com>
2019-03-01 14:20:26 -0500

    Clean up and update submodules

--- launch_simulation.sh
+++ launch_simulation.sh
@@ -7,9 +7,19 @@ mkdir buzz/build
 cd buzz/build
 cmake ../src
 sudo make install
+cd ../..
+
+# Build Robust Distributed Mapper
+cd robust_distributed_mapper/cpp
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+cd ../../..
 
 # Compile Buzz script
-cd ../../argos_simulation
+cd argos_simulation
 bzzc random_walk_mapping.bzz
 
 # Launch simulation
